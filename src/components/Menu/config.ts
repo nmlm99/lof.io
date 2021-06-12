8d33b2f9fe897033532096778e77deaf91f6ccb7
nmlm99 <Noahmooretcg@gmail.com>
2021-06-11 21:55:46 -0500

    initial

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,104 @@
+import { MenuEntry } from 'lof-uikit'
+
+const config: MenuEntry[] = [
+
+  {
+    label: 'Exchange',
+    icon: 'TradeIcon',
+    href: '/swap',
+  },
+  {
+    label: 'Liquidity',
+    icon: 'PoolIcon',
+    href: '/pool'
+  }
+  // initialOpenState: true,
+  //   items: [
+  //     {
+  //       label: 'Exchange',
+  //       href: '/swap'
+  //     },
+  //     {
+  //       label: 'Liquidity',
+  //       href: '/pool'
+  //     }
+  //   ]
+  // },
+  // {
+  //   label: 'Partnerships/IFO',
+  //   icon: 'GooseIcon',
+  //   href: 'https://forms.gle/Y9B6xEAQe4asakiM8',
+  // },
+  // {
+  //   label: 'Farms',
+  //   icon: 'FarmIcon',
+  //   href: 'https://www.i.com/farms'
+  // },
+  // {
+  //   label: 'Bowls',
+  //   icon: 'PoolIcon',
+  //   href: 'https://www.fi.com/bowls'
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.goosedefi.com/lottery'
+  // },
+  // {
+  //   label: 'Info',
+  //   icon: 'InfoIcon',
+  //   items: [
+  // {
+  //   label: 'PancakeSwap',
+  //  href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+  // },
+  // {
+  //   label: 'CoinGecko',
+  //   href: 'https://www.coingecko.com/en/coins/goose-finance',
+  // },
+  // {
+  //   label: 'CoinMarketCap',
+  //   href: 'https://coinmarketcap.com/currencies/goose-finance/',
+  // },
+  // {
+  //   label: 'AstroTools',
+  //   href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+  // },
+  //   ]
+  // },
+  // {
+  //   label: 'More',
+  //   icon: 'MoreIcon',
+  //   items: [
+  // {
+  //   label: "Github",
+  //   href: "https://github.comefi/",
+  // },
+  // {
+  //   label: "Docs",
+  //   href: "https://fi.gitbook.io/finance/",
+  // },
+  // {
+  //   label: "Blog",
+  //   href: "https://goosefinance.medium.com/",
+  // },
+  //   ],
+  // },
+  // {
+  //   label: 'Partnerships/IFO',
+  //   icon: 'GooseIcon',
+  //   href: 'https://forms.gle/Y9B6xEAQe4asakiM8',
+  // },
+  //  {
+  //    label: 'Audit by Hacken',
+  //    icon: 'AuditIcon',
+  //    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+  //  },
+  //  {
+  //    label: 'Audit by CertiK',
+  //    icon: 'AuditIcon',
+  //    href: 'https://certik.org/projects/goose-finance',
+  //  },
+]
+
+export default config
